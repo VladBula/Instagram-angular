2a15496189f229c20ae0bf92783704ab1e12207d
VladBula <vladbulynk@gmail.com>
2023-10-09 13:47:50 +0200

    delT

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,6 +1,22 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
+export interface CreateTodoResponse {
+  data: {
+    item: Todo
+  };
+  messages: string[]
+  fieldsErrors: string[]
+  resultCode: number
+}
+
+export interface DeleteTodoResponse {
+  data: {}
+  messages: string[]
+  fieldsErrors: string[]
+  resultCode: number
+}
+
 interface Todo {
   id: string,
   title: string,
@@ -21,6 +37,12 @@ interface Todo {
 export class TodosComponent implements OnInit {
 
   todos: Todo[] = []
+  httpOptions = {
+    withCredentials: true,
+    headers: {
+      'api-key': '32715ae4-a491-4357-baa9-372f740761e4',
+    },
+  }
 
   constructor(private http: HttpClient) {
   }
@@ -29,14 +51,26 @@ export class TodosComponent implements OnInit {
     this.getTodos()
   }
 
-  private getTodos() {
-    this.http.get<Todo[]>('https://social-network.samuraijs.com/api/1.1/todo-lists', {
-      withCredentials: true,
-      headers: {
-        'api-key': '32715ae4-a491-4357-baa9-372f740761e4',
-      },
-    }).subscribe((res) => {
+  getTodos() {
+    this.http.get<Todo[]>('https://social-network.samuraijs.com/api/1.1/todo-lists', this.httpOptions).subscribe((res) => {
       this.todos = res
     })
+  }
+
+  createTodo() {
+    const randomNumber = Math.floor(Math.random() * 100)
+    const title = "ang" + randomNumber
+    this.http.post<CreateTodoResponse>('https://social-network.samuraijs.com/api/1.1/todo-lists', {title}, this.httpOptions).subscribe(res => {
+      this.todos.unshift(res.data.item)
+    })
+  }
+
+  deleteTodo() {
+    const todoId ='783d4ca2-1363-40a6-ad5c-5501872be830'
+    this.http.delete<DeleteTodoResponse>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todoId}`, this.httpOptions)
+      .subscribe((res)=>{
+        if (res.resultCode === 0)
+        this.todos = this.todos.filter(tl => tl.id !== todoId)
+      })
   }
 }
