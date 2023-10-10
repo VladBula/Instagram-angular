7251bb07c43f71bb955210a6944b243ee9bee177
VladBula <vladbulynk@gmail.com>
2023-10-10 13:01:26 +0200

    refactoring_BS

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Todo, TodosService} from "../../services/todos.service";
-
+import {HttpErrorResponse} from "@angular/common/http";
+import {Observable, Subscription} from "rxjs";
 
 
 // "id": "7b659935-98e7-423e-8f40-2c2a7db62460",
@@ -13,38 +14,76 @@ import {Todo, TodosService} from "../../services/todos.service";
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss']
 })
-export class TodosComponent implements OnInit {
+export class TodosComponent implements OnInit{
 
-  todos: Todo[] = []
-
+  todos$!: Observable<Todo[]>
+  error = ''
 
   constructor(private todosService: TodosService) {
   }
 
   ngOnInit(): void {
+    //subscribe
+    this.todos$ = this.todosService.todos$
     this.getTodos()
   }
 
+  _getTodos() {
+    // this.subscriptions.add(
+    //   this.todosService.getTodos().subscribe(
+    //     {
+    //       next: (res) => {
+    //         this.todos = res
+    //       },
+    //       error: (error: HttpErrorResponse) => {
+    //         this.error = error.message
+    //       }
+    //     })
+    // )
+  }
+
   getTodos() {
-    this.todosService.getTodos().subscribe((res) => {
-      this.todos = res
-    })
+    this.todosService.getTodos()
+      // this.todosService.getTodos().subscribe(
+      //   {
+      //     next: (res) => {
+      //       this.todos = res
+      //     },
+      //     error: (error: HttpErrorResponse) => {
+      //       this.error = error.message
+      //     }
+      //   })
+
   }
 
   createTodo() {
     const randomNumber = Math.floor(Math.random() * 100)
     const title = "ang" + randomNumber
-    this.todosService.createTodo(title).subscribe(res => {
-      this.todos.unshift(res.data.item)
-    })
+
+    this.todosService.createTodo(title)
+
+    // this.subscriptions.add(
+    //   this.todosService.createTodo(title).subscribe(res => {
+    //     this.todos.unshift(res.data.item)
+    //   })
+    // )
   }
 
   deleteTodo() {
-    const todoId ='b6049a15-5510-45ca-839d-2f283e489513'
-   this.todosService.deleteTodo(todoId)
-      .subscribe((res)=>{
-        if (res.resultCode === 0)
-        this.todos = this.todos.filter(tl => tl.id !== todoId)
-      })
+
+
+    const todoId = 'cc8064a6-d308-4595-8725-d3297a27a1e6'
+
+    this.todosService.deleteTodo(todoId)
+    // this.subscriptions.add(
+    //   this.todosService.deleteTodo(todoId)
+    //     .subscribe((res) => {
+    //       if (res.resultCode === 0)
+    //         this.todos = this.todos.filter(tl => tl.id !== todoId)
+    //     })
+    // )
+
   }
+
+
 }
--- src/services/todos.service.ts
+++ src/services/todos.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../environments/environment";
+import {BehaviorSubject, map} from "rxjs";
 
 export interface TodoResponse<T = {}> {
   data: T;
@@ -21,6 +22,8 @@ export interface Todo {
 })
 export class TodosService {
 
+  todos$ = new BehaviorSubject<Todo[]>([])
+
   httpOptions = {
     withCredentials: true,
     headers: {
@@ -32,15 +35,44 @@ export class TodosService {
   }
 
   getTodos() {
-    return this.http.get<Todo[]>(`${environment.baseUrl}/todo-lists`, this.httpOptions)
+    this.http.get<Todo[]>(`${environment.baseUrl}/todo-lists`, this.httpOptions)
+      .subscribe(todos => {
+        this.todos$.next(todos)
+      })
   }
 
   createTodo(title: string) {
-    return this.http.post<TodoResponse<{item: Todo}>>(`${environment.baseUrl}/todo-lists`,
+    this.http.post<TodoResponse<{ item: Todo }>>(`${environment.baseUrl}/todo-lists`,
       {title}, this.httpOptions)
+      .pipe(map(
+        (data) => {
+          const newTodo = data.data.item
+          const stateTodos = this.todos$.getValue()
+          return [newTodo, ...stateTodos]
+        }
+      ))
+      .subscribe((todos) => {
+
+        this.todos$.next(todos)
+      })
   }
 
-  deleteTodo(todoId:string) {
-   return this.http.delete<TodoResponse>(`${environment.baseUrl}/todo-lists/${todoId}`, this.httpOptions)
+  deleteTodo(todoId: string) {
+    this.http.delete<TodoResponse>(`${environment.baseUrl}/todo-lists/${todoId}`, this.httpOptions)
+      .pipe(map(
+        (res) => {
+          if (res.resultCode === 0) {
+            return this.todos$.getValue().filter(tl => tl.id !== todoId)
+          } else {
+            return this.todos$.getValue()
+          }
+
+        }
+      )).subscribe(
+      (todos) => {
+
+        this.todos$.next(todos)
+      }
+    )
   }
 }
